0ae34a85e67ed59fa0e98abb89f1fe2cefc1a96c
Dave Bartolomeo <dbartol@github.com>
2023-05-11 19:22:26 -0400

    Update Install-SshKey.ps1

--- Install-SshKey.ps1
+++ Install-SshKey.ps1
@@ -1,3 +1,8 @@
-Connect-AzAccount -Identity
-$Key = Get-AzSshKey -Name cluster-ssh -ResourceGroup bob-dev3
-$Key.publicKey | Out-File "C:\key.pub"
+&{
+  'Connecting to Azure'
+  Connect-AzAccount -Identity -ErrorAction Continue
+  'Getting SSH key'
+  $Key = Get-AzSshKey -Name cluster-ssh -ResourceGroup bob-dev3 -ErrorAction Continue
+  'Storing SSH key'
+  $Key.publicKey | Out-File "C:\key.pub"
+} >C:\log.txt 2>&1
